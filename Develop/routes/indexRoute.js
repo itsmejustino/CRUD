5a92b1b254f5a476afa5d5cebe290a0f60280814
jdog <thuz3rfr13ndly@gmail.com>
2022-07-13 14:25:40 -0600

    remove uneeded syntax to route

--- Develop/routes/indexRoute.js
+++ Develop/routes/indexRoute.js
@@ -1,9 +1,7 @@
-const noteRoute = require('/notes');
-//user path if helper needed
-const { Router } = require('router');
-const { application } = require('express');
-const router  = Router();
-
-app.use('/notes', noteRoute);
-
+//require path to notes
+const noteRoute = require("/notes");
+const { Router } = require("express");
+const router = Router();
+app.use("/notes", noteRoute);
+//export notes to router.
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,48 +1,14 @@
-const express = require('express');
-const router = require('router');
-//user path if helper needed
-// const path = require('path');
-// const api = require('./db')
-// const { Router } = require
-const PORT = process.env.PORT || 3001;
-
+const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+const api = require("./routes/noteRoute");
+app.use("/api", api);
 
-app.post('./db.json', (req, input) => {
-console.info(`${req.method} request received to add a note`);
-const {title, text} = req.body;
- if (title && text) {
-   const addNote = {
-     title,
-     text,
-    //  note_id,
-   };
-   const postNote = JSON.parse(input);
-   postNote.push(addNote);
-
-   fs.writeFile("./db/db.json", JSON.stringify(postNote), (err) =>
-     err ? console.log(err) : console.log("Added a note! =)")
-   );
- 
-//handles error response message to terminal tom indicate a successful posted note.
-const responseStatus = {
-    status:'Its a go!',
-    body: addNote,
-};
-console.log(responseStatus); 
-res.status(201).json(response);
- }else{
-    res.status(500).json("Error in posting note! ='( ");
- }
-
-    });
-
-app.listen(PORT , () =>console.log('server listening on port:'+`http://localhost:${PORT}`) );
+app.listen(PORT, () =>
+  console.log("server listening on port:" + `http://localhost:${PORT}`)
+);
